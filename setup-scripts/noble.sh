25cbaa60c9f05b4b8459e4f59b2ef6820f44adcf
Johannes Grassler <info@computer-grassler.de>
2024-08-22 13:01:46 +0000

    Add CloudCIX to cloud-init data source configuration.

--- setup-scripts/noble.sh
+++ setup-scripts/noble.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 dpkg -i --auto-deconfigure /root/cloud-init_24.2-0ubuntu2~24.04.2_all.deb
+cat <<EOF > /etc/cloud/cloud.cfg.d/90_dpkg.cfg
+# to update this file, run dpkg-reconfigure cloud-init
+datasource_list: [ NoCloud, ConfigDrive, OpenNebula, DigitalOcean, Azure, AltCloud, OVF, MAAS, GCE, OpenStack, CloudSigma, SmartOS, Bigstep, Scaleway, AliYun, Ec2, CloudCIX, CloudStack, Hetzner, IBMCloud, Oracle, Exoscale, RbxCloud, UpCloud, VMware, Vultr, LXD, NWCS, Akamai, WSL, None ]
+EOF
